df383b91412bc304b481b10620ad532e606e8dbd
Shashank Chaudhry <shashank.chaudhry@gmail.com>
2015-06-09 00:03:22 -0500

    organization

--- src/MarkerDetector.hpp
+++ src/MarkerDetector.hpp
@@ -1,14 +1,4 @@
-/*****************************************************************************
-*   MarkerDetector.hpp
-*   Example_MarkerBasedAR
-******************************************************************************
-*   by Khvedchenia Ievgen, 5th Dec 2012
-*   http://computer-vision-talks.com
-******************************************************************************
-*   Ch2 of the book "Mastering OpenCV with Practical Computer Vision Projects"
-*   Copyright Packt Publishing 2012.
-*   http://www.packtpub.com/cool-projects-with-opencv/book
-*****************************************************************************/
+
 
 #ifndef Example_MarkerBasedAR_MarkerDetector_hpp
 #define Example_MarkerBasedAR_MarkerDetector_hpp
